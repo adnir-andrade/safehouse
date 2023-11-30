08702dd147b87a1a0f870d2b263e3ce35c5bd63e
adnir-andrade <adnir_andrade@hotmail.com>
2023-11-30 16:52:00 -0300

    Refactor navbar into jsx component

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,27 +1,46 @@
-export default function Navbar() {
+export default function Navbar({ onClick }) {
   return (
     <div className="navbar bg-base-100">
-      <div class="flex-none">
+      <div className="flex-none">
         <button
-          htmlFor="my-drawer-2"
-          class="btn btn-square btn-ghost drawer-button lg:hidden"
+          //   htmlFor="my-drawer-2"
+          className="btn btn-square btn-ghost drawer-button lg:hidden"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            class="inline-block w-5 h-5 stroke-current"
+            className="inline-block w-5 h-5 stroke-current"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             ></path>
           </svg>
         </button>
       </div>
-      <a className="btn btn-ghost text-xl">The Safehouse</a>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">The Safehouse</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
