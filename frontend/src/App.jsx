c2fa424bfffe967c5f66af49d22bd8edb72274d6
adnir-andrade <adnir_andrade@hotmail.com>
2023-11-30 16:47:35 -0300

    Remove unused content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./index.css";
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isChecked, setIsChecked] = useState(false);
+
+  function changeState() {
+    setIsChecked(!isChecked);
+  }
 
   return (
     <>
-      <Navbar />
+      <Navbar onClick={changeState} />
       <Sidebar>
         <div className="flex flex-col w-full lg:flex-row">
           <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
