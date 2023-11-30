f69bafb1639d2e853a9b661fdd8bc462dade8642
adnir-andrade <adnir_andrade@hotmail.com>
2023-11-30 14:35:15 -0300

    Daisyui configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <div className="App">
+        <button className="btn">Hello daisyUI</button>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
