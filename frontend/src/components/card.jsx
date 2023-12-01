2349eb95540141d43fbd30559c95740c17d5970c
adnir-andrade <adnir_andrade@hotmail.com>
2023-12-01 16:14:36 -0300

    Adjust card opacity

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ title = "", children }) {
   return (
-    <div className="card w-96 bg-neutral text-neutral-content">
+    <div className="card w-96 bg-neutral bg-opacity-90 text-neutral-content">
       <div className="card-body items-center text-center">
         <h2 className="card-title">{title}</h2>
         {children}
