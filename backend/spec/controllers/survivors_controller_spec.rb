f7c05ca64be6ab22bc043f56962173c3775735ba
Adnir Andrade <adnir_andrade@hotmail.com>
2023-12-11 09:50:11 -0300

    Feat: Implement basic test for Suvivor controller

--- backend/spec/controllers/survivors_controller_spec.rb
+++ backend/spec/controllers/survivors_controller_spec.rb
@@ -3,12 +3,27 @@ require 'rails_helper'
 RSpec.describe SurvivorsController, type: :controller do
   describe 'GET #index' do
     it 'returns all survivors' do
-      create_survivors
+      create_list(:survivor, 10);
 
       get :index, format: :json
 
-      # expect(response).to have_http_status(200)
-      # expect(JSON.parse(response.body))
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body, {
+        symbolize_names: true,
+      })[:survivors]).to eq(
+        Survivor.all.map { |e| { 
+          id: e.id, 
+          name: e.name, 
+          gender: e.gender, 
+          age: e.age, 
+          is_alive: "Alive",
+          locations: [], 
+          created_at: e.created_at.strftime('%Y-%m-%d'),
+          updated_at: e.updated_at.strftime('%Y-%m-%d'),
+          inventory_id: nil,
+          location_id: nil
+         } }
+      )
     end
   end
 end
--- backend/spec/factories/survivors.rb
+++ backend/spec/factories/survivors.rb
@@ -1,13 +1,12 @@
 require 'faker'
 
-FactoryBot.defined do
+FactoryBot.define do
   factory :survivor do
     name { Faker::Name.name }
-    age
-    gender
-    is_alive
-    longitude
-    latitude
-    # TODO: Create blueprint here
+    age { Faker::Number.between(from: 15, to: 90)}
+    gender { Faker::Gender.binary_type }
+    is_alive { "Alive" }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
