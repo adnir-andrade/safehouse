11c673025468649ae4c00295ce82d954e8b786d4
Adnir Andrade <adnir_andrade@hotmail.com>
2023-12-27 08:25:22 -0300

    Test: Implements POST tests

--- backend/spec/controllers/items_controller_spec.rb
+++ backend/spec/controllers/items_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+  let(:item) { attributes_for(:item)}
 
   describe 'GET #index' do
     it 'returns all items' do
@@ -27,6 +28,56 @@ RSpec.describe ItemsController, type: :controller do
 
       validate_success(response, 200, 1)
       expect(responseJSON).to eq(expectedJSON)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'using valid attributes' do
+      it 'creates a new item' do
+        post :create, params: { item: item }
+
+        validate_success(response, 201, 1)
+      end
+
+      context 'for item' do
+        it 'creates an item with min valid value' do
+          post :create, params: { item: item.merge(value: 0.01) }
+
+          validate_success(response, 201, 1)
+        end
+      end
+
+      context 'for description' do
+        it 'creates an item without description' do
+          post :create, params: { item: item.merge(description: nil) }
+
+          validate_success(response, 201, 1)
+        end
+      end
+    end
+
+    context 'using invalid attributes' do
+      context 'for name' do
+        it "doesn't create an item when name is nil or empty" do
+          post :create, params: { item: item.merge(name: nil) }
+
+          validate_success(response, 422)
+        end
+      end
+
+      context 'for value' do
+        it "doesn't create an item when value is nil or empty" do
+          post :create, params: { item: item.merge(value: nil) }
+
+          validate_success(response, 422)
+        end
+
+        it "doesn't create an item when value is below min" do
+          post :create, params: { item: item.merge(value: 0) }
+
+          validate_success(response, 422)
+        end
+      end
     end
   end
 
