87cab0a3fed4bef5cbc20cc99e15020c566c8ae7
Adnir Andrade <adnir_andrade@hotmail.com>
2023-12-27 09:20:05 -0300

    Test: Implements PUT tests

--- backend/spec/controllers/items_controller_spec.rb
+++ backend/spec/controllers/items_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  let(:item) { attributes_for(:item)}
+  let(:item) { attributes_for(:item) }
+  let(:standard_item) { create(:item) }
 
   describe 'GET #index' do
     it 'returns all items' do
@@ -76,6 +77,59 @@ RSpec.describe ItemsController, type: :controller do
           post :create, params: { item: item.merge(value: 0) }
 
           validate_success(response, 422)
+        end
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      it 'updates item with valid name' do
+        put :update, params: { id: standard_item.id, item: { name: 'Bazooka' } }
+
+        validate_success(response, 200, 1)
+        expect(standard_item.reload.name).to eq('Bazooka')
+      end
+
+      it 'updates item with valid value' do
+        put :update, params: { id: standard_item.id, item: { value: 77.98 } }
+
+        validate_success(response, 200, 1)
+        expect(standard_item.reload.value).to eq(77)
+      end
+
+      it "updates item's description" do
+        put :update, params: { id: standard_item.id, item: { description: "That's probably gonna hurt if you point to the wrong direction"} }
+
+        validate_success(response, 200, 1)
+        expect(standard_item.reload.description).to eq("That's probably gonna hurt if you point to the wrong direction")
+      end
+    end
+
+    context 'with invalid params' do
+      it "doesn't update item when name is empty or nil" do
+        original_name = standard_item.name
+        put :update, params: { id: standard_item.id, item: { name: nil } }
+
+        validate_success(response, 422, 1)
+        expect(standard_item.reload.name).to eq(original_name)
+      end
+
+      context 'for value' do
+        it "doesn't update item when value is empty or nil" do
+          original_value = standard_item.value
+          put :update, params: { id: standard_item.id, item: { value: nil } }
+
+          validate_success(response, 422, 1)
+          expect(standard_item.reload.value).to eq(original_value)
+        end
+
+        it "doesn't update item when value is below minimum" do
+          original_value = standard_item.value
+          put :update, params: { id: standard_item.id, item: { value: 0 } }
+  
+          validate_success(response, 422, 1)
+          expect(standard_item.reload.value).to eq(original_value)
         end
       end
     end
