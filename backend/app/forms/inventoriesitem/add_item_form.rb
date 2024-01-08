7ba66e730fa410f7a145a6ff317c202058db4735
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-08 05:19:09 -0300

    Refactor: Divided add-item and remove-quantity into two different forms

--- backend/app/forms/inventoriesitem/add_item_form.rb
+++ backend/app/forms/inventoriesitem/add_item_form.rb
@@ -1,4 +1,4 @@
-class Inventoriesitem::ItemManagementForm
+class Inventoriesitem::AddItemForm
   include ActiveModel::Model
 
   attr_accessor :inventory_id, :item_id, :quantity, :inventoryitem
@@ -7,33 +7,24 @@ class Inventoriesitem::ItemManagementForm
   validates :item_id, presence: true
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 
+  def quantity=(value)
+    @quantity = value.to_i
+  end
+
   def add_quantity
-    puts "Trying to add a new item"
     return false if invalid?
 
     inventory_item = InventoriesItem.find_or_initialize_by(item_id: item_id, inventory_id: inventory_id)
     store_item(inventory_item)
   end
 
-  def remove_quantity
-    return false if invalid?
-
-    if @inventoryitem.quantity - @quantity > 0
-      @quantity *= -1
-      store_item(@inventoryitem)
-    else
-      errors.add(:quantity, 'Not enough quantity in the inventory to be removed')
-      return false
-    end
-  end
-
   private
 
   def store_item(inventory_item)
     if inventory_item.persisted?
-      inventory_item.quantity += quantity.to_i
+      inventory_item.quantity += quantity
     else
-      inventory_item.quantity = quantity.to_i
+      inventory_item.quantity = quantity
     end
 
     if inventory_item.save
---
+++ backend/app/forms/inventoriesitem/remove_quantity_form.rb
@@ -0,0 +1,38 @@
+class Inventoriesitem::RemoveQuantityForm
+  include ActiveModel::Model
+
+  attr_accessor :inventoryitem, :quantity
+
+  validates :quantity, presence: true, numericality: { only_integer: true }
+  validate :has_enough?
+
+  def quantity=(value)
+    @quantity = value.to_i
+  end
+
+  def remove_quantity
+    puts "Trying to remove quantity from an item"
+    return false if invalid?
+
+    update_quantity(@inventoryitem)
+  end
+
+  private
+
+  def update_quantity(inventory_item)
+    inventory_item.quantity -= quantity
+
+    if inventory_item.save
+      return true
+    else
+      errors.merge!(item.errors)
+      return false
+    end
+  end
+
+  def has_enough?
+    return if @inventoryitem.quantity - @quantity.abs > 0
+
+    errors.add(:quantity, 'Not enough quantity in the inventory to be removed')
+  end
+end
