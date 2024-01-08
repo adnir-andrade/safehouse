dac6c6c0119154b0316404e4ae7263937eece48d
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-08 05:39:32 -0300

    Refactor: Removed empty spaces

--- backend/spec/controllers/inventoriesitem_controller_spec.rb
+++ backend/spec/controllers/inventoriesitem_controller_spec.rb
@@ -96,7 +96,7 @@ RSpec.describe InventoriesitemController, type: :controller do
         existing_entry = InventoriesItem.create(standard_entry.merge(quantity: 10))
         put :remove_quantity, params: { id: existing_entry.id, inventoriesitem: { quantity: 3 } }
         
-        expect(existing_entry.reload.quantity).to eq(7)        
+        expect(existing_entry.reload.quantity).to eq(7)
         validate_success(response, 200, 1)
       end
 
@@ -104,7 +104,7 @@ RSpec.describe InventoriesitemController, type: :controller do
         existing_entry = InventoriesItem.create(standard_entry.merge(quantity: 10))
         put :remove_quantity, params: { id: existing_entry.id, inventoriesitem: { quantity: -3 } }
         
-        expect(existing_entry.reload.quantity).to eq(7)        
+        expect(existing_entry.reload.quantity).to eq(7)
         validate_success(response, 200, 1)
       end
     end
