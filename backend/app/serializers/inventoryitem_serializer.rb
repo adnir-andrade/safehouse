ddcf980301a7eca3f3073f3b262db9e16effa9c2
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-08 06:54:57 -0300

    Refactor: Changed indentation for more readability

--- backend/app/serializers/inventoryitem_serializer.rb
+++ backend/app/serializers/inventoryitem_serializer.rb
@@ -3,11 +3,19 @@ class InventoryitemSerializer < ActiveModel::Serializer
   attributes :id, :item, :owner
 
   def item
-    { id: object.item.id, name: object.item.name, quantity: object.quantity }
+    { 
+      id: object.item.id, 
+      name: object.item.name, 
+      quantity: object.quantity 
+    }
   end
 
   def owner
-    { id: object.inventory.survivor.id, name: object.inventory.survivor.name, inventory_id: object.inventory_id }
+    { 
+      id: object.inventory.survivor.id, 
+      name: object.inventory.survivor.name, 
+      inventory_id: object.inventory_id 
+    }
   end
 
 end
