ee8c66066bb5facc054e7633b7aefbb4e63f5be8
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-11 10:39:04 -0300

    Refactor: Sort PDF in alphabetical order

--- backend/app/controllers/reports_controller.rb
+++ backend/app/controllers/reports_controller.rb
@@ -2,7 +2,9 @@ class ReportsController < ApplicationController
 
   def survivors_report
     survivors = Survivor.pluck(:name, :gender, :age)
-    pdf = SurvivorsPdf.new(survivors: survivors)
+    # TODO: Make a switch or something else to sort conditional to what the user wants
+    sorted_survivors = survivors.sort_by { |survivor| survivor.first }
+    pdf = SurvivorsPdf.new(survivors: sorted_survivors)
     pdf.render_document
 
     respond_to do |format|
