a648dfc6d0f8f7d236372ed060e70f47cf4a9f00
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-12 13:26:19 -0300

    Refactor: Fix indentation

--- backend/app/controllers/items_report_controller.rb
+++ backend/app/controllers/items_report_controller.rb
@@ -1,17 +1,17 @@
 class ItemsReportController < ReportsController
     def items_report
         generate_report("item", Item, [:name, :value, :description], ItemsPdf, method(:sort_items))
-      end
+    end
     
-      def sort_items(items)
+    def sort_items(items)
         filters = {
-          "name-asc" => -> { items.sort_by! { |item| item[0] } },
-          "name-desc" => -> { items.sort_by! { |item| item[0] }.reverse! },
-          "value-asc" => -> { items.sort_by! { |item| item[1] } },
-          "value-desc" => -> { items.sort_by! { |item| item[1] }.reverse! },
-          "description" => -> { items.sort_by! { |item| item[2] } },
+            "name-asc" => -> { items.sort_by! { |item| item[0] } },
+            "name-desc" => -> { items.sort_by! { |item| item[0] }.reverse! },
+            "value-asc" => -> { items.sort_by! { |item| item[1] } },
+            "value-desc" => -> { items.sort_by! { |item| item[1] }.reverse! },
+            "description" => -> { items.sort_by! { |item| item[2] } },
         }
-    
+
         filters.fetch(@sorter, -> { return }).call
-      end
+    end
 end
