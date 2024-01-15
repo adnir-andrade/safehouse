94be779585eb47ba8e9b6a962a995d9f88196083
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-15 13:08:19 -0300

    Refactor: Encapsuled everything related to PDF inside format.pdf

--- backend/app/controllers/reports_controller.rb
+++ backend/app/controllers/reports_controller.rb
@@ -4,11 +4,11 @@ class ReportsController < ApplicationController
   def generate_report(entity_name, entity_class, entity_attributes, pdf_class, sort_method)
     data = entity_class.pluck(entity_attributes)
     sort_method.call(data)
-    pdf = pdf_class.new(data: data)
-    pdf.render_document
-
+    
     respond_to do |format|
       format.pdf do
+        pdf = pdf_class.new(data: data)
+        pdf.render_document
         send_data(pdf.render, filename: "#{entity_name}.pdf", type: "application/pdf", disposition: "inline")
       end
     end
