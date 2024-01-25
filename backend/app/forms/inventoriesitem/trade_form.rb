bb0330026778d5c7c03b0376824cebbc84146e69
Adnir Andrade <adnir_andrade@hotmail.com>
2024-01-25 12:51:43 -0300

    Feat: Implemented functional trade of items (no currency involved) and dynamic error returns. Needs refactor, but I am so proud of myself rn <3

--- backend/app/forms/inventoriesitem/trade_form.rb
+++ backend/app/forms/inventoriesitem/trade_form.rb
@@ -6,9 +6,6 @@ class Inventoriesitem::TradeForm
   validates :items_wanted, presence: true
   validates :items_offered, presence: true
 
-  # validates :quantity, presence: true, numericality: { only_integer: true }
-  # validate :has_enough?
-
   # def quantity=(value)
   #   @quantity = value.to_i.abs
   # end
@@ -21,32 +18,83 @@ class Inventoriesitem::TradeForm
     @seller = Survivor.find(id)
   end
 
-  def start_trade
+  def initialize(attributes = {})
+    super
     @buyer_items = set_trade_items(buyer, items_offered)
     @seller_items = set_trade_items(seller, items_wanted)
+  end
+
+  def start_trade
     return false if !has_enough_wanted_items?
     return false if invalid?
-    return true
+    exchange_items
   end
 
   private
 
-  def update_quantity(inventory_item)
-    inventory_item.quantity -= quantity
-    if inventory_item.save
-      return true
-    else
-      errors.merge!(item.errors)
-      return false
+  def exchange_items
+    InventoriesItem.transaction do
+      puts "GETTING ITEMS FROM VENDOR TO BUYER"
+      add_item_forms = []
+      remove_quantity_forms = []
+      test = InventoriesItem.new()
+      items_wanted.each { |item| 
+        add_item_forms << Inventoriesitem::AddItemForm.new({inventory_id: @buyer[:inventory_id], item_id: item[:item_id], quantity: item[:quantity]})
+        # TODO: Change this part later to deal with stack size
+        seller_items = @seller_items.select { |i| i.item_id == item[:item_id]}
+        remove_quantity_forms << Inventoriesitem::RemoveQuantityForm.new({inventoryitem: seller_items.first, quantity: item[:quantity]})
+      }
+
+      unless add_item_forms.all?(&:add_quantity) && remove_quantity_forms.all?(&:remove_quantity)
+        errors.add(:trade, 'There was a problem while trying to make the exchange')
+        raise ActiveRecord::Rollback
+        return false
+      end
+
+      puts "GETTING ITEMS FROM BUYER TO VENDOR"
+      add_item_forms = []
+      remove_quantity_forms = []
+      items_offered.each { |item| 
+        # TODO: Change this part later to deal with stack size
+        add_item_forms << Inventoriesitem::AddItemForm.new({inventory_id: @seller[:inventory_id], item_id: item[:item_id], quantity: item[:quantity]})
+        buyer_items = @buyer_items.select { |i| i.item_id == item[:item_id]}
+        remove_quantity_forms << Inventoriesitem::RemoveQuantityForm.new({inventoryitem: buyer_items.first, quantity: item[:quantity]})
+      }
+
+      unless add_item_forms.all?(&:add_quantity) && remove_quantity_forms.all?(&:remove_quantity)
+        add_item_forms.each_with_index do |form, i|
+          if !form.errors.empty?
+            errors.add(:remove_quantity, "Couldn't add item using add_item_form of index #{i} as following:")
+            form.errors.details.each do |key, details|
+              details.each do |detail|
+                error = detail[:error]
+                errors.add(key, error)
+              end
+            end
+          end
+        end
+        
+        remove_quantity_forms.each_with_index do |form, i|
+          if !form.errors.empty?
+            errors.add(:remove_quantity, "Couldn't remove quantity using remove_quantity_form of index #{i} as following:")
+            form.errors.details.each do |key, details|
+              details.each do |detail|
+                error = detail[:error]
+                errors.add(key, error)
+              end
+            end
+          end
+        end
+        
+        errors.add(:trade, 'There was a problem while trying to make the exchange')
+        raise ActiveRecord::Rollback
+        return false
+      end
     end
+    
+    return true
   end
-
-  def has_enough?
-    return if @inventoryitem.quantity - @quantity > 0
-
-    errors.add(:quantity, 'Not enough quantity in the inventory to be removed')
-  end
-
+  
   def set_trade_items(survivor, items)
     items_id = []
     items.each { |item|
@@ -55,12 +103,15 @@ class Inventoriesitem::TradeForm
     return InventoriesItem.where("inventory_id = #{survivor.inventory_id} AND item_id IN (#{items_id.join(', ')})")
   end
 
+  # TODO: Apparently, gotta check from the fucking buyer as well
   def has_enough_wanted_items?
     items_wanted.each { |item_wanted|
       seller_items = @seller_items.select { |i| i.item_id == item_wanted[:item_id]}
       items_in_stock = get_quantity(seller_items)
 
       if item_wanted[:quantity] > items_in_stock
+        # Alternatively: Would be a good idea to set the desired quantity to the max. quantity available in this case.
+        # But I think, for now, this is good.
         errors.add(:test, 'Not enough quantity in the inventory of the SELLER to be traded')
         return false
       end
